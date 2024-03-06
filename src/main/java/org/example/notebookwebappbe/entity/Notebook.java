e3ca398bab58a14d006f9ebdc03b255b0e06202f
Ahmet Sami Yeniçeri <ahmet.sami.yeniceri@ericsson.com>
2024-03-07 02:05:03 +0300

    format arrangement

--- src/main/java/org/example/notebookwebappbe/entity/Notebook.java
+++ src/main/java/org/example/notebookwebappbe/entity/Notebook.java
@@ -1,6 +1,5 @@
 package org.example.notebookwebappbe.entity;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
